f5873a4725e8a87a0db9360116525961690307db
alirezamhm <alireza.am1379@gmail.com>
2019-04-19 20:39:14 +0430

    csvReader updated

--- src/control/CsvReader.java
+++ src/control/CsvReader.java
@@ -1,4 +1,25 @@
 package control;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class CsvReader {
+    public static ArrayList<String[]> ReadCards(String cardType){
+        ArrayList<String[]> data = new ArrayList<>();
+        String fileAddress = "Files/" + cardType +".csv";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileAddress))) {
+            String line = reader.readLine();
+
+            while ((line = reader.readLine()) != null) {
+                data.add(line.split(","));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
 }
+
