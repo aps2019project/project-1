91780bc2dac84f3f355e4eeeaca06cd7821d161a
alirezamhm <alireza.am1379@gmail.com>
2019-04-28 15:19:34 +0430

    Army class added (Hero and Minion extends Army)

---
+++ src/model/cards/Army.java
@@ -0,0 +1,29 @@
+package model.cards;
+
+public class Army extends Card {
+    protected int hp, ap, ar;
+    protected AttackType attackType;
+    public Army (String name, int price, String description, int hp, int ap, int ar, AttackType attackType){
+        super(name, price, description);
+        this.hp = hp;
+        this.ap = ap;
+        this.ar = ar;
+        this.attackType = attackType;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getAp() {
+        return ap;
+    }
+
+    public int getAr() {
+        return ar;
+    }
+
+    public AttackType getAttackType() {
+        return attackType;
+    }
+}
