6dae88d54c14bee617c2e91d40ae4a9e58ae847f
erfanmotnt <moeini.erfan@gmail.com>
2019-06-14 00:33:37 +0430

    test git

--- core/src/model/other/Account.java
+++ core/src/model/other/Account.java
@@ -7,7 +7,7 @@ import model.cards.Card;
 import model.game.Deck;
 import model.game.MatchResult;
 import model.variables.CardsArray;
-
+//test/ erfan
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
