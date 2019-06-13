0e9459c5ba200692bac035c39fa270b90947351e
alirezamhm <alireza.am1379@gmail.com>
2019-06-13 17:02:30 +0430

    file addresses changed

--- core/src/control/CsvReader.java
+++ core/src/control/CsvReader.java
@@ -9,7 +9,7 @@ public class CsvReader {
 
     public static ArrayList<String[]> readCards(String cardType){
         ArrayList<String[]> data = new ArrayList<String[]>();
-        String fileAddress = "core/assets/Files/" + cardType +".csv";
+        String fileAddress = "Files/" + cardType +".csv";
         try {
             FileReader fileReader = new FileReader(fileAddress);
             BufferedReader reader = new BufferedReader(fileReader);
