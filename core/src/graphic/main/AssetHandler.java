1e2d804c59b0899c730231ba5e36cd3a21ff0943
sadraheydari <msadrah521@gmail.com>
2019-06-13 19:28:20 +0430

    create signUp and Login Button

--- core/src/graphic/main/AssetHandler.java
+++ core/src/graphic/main/AssetHandler.java
@@ -1,6 +1,8 @@
 package graphic.main;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
 public class AssetHandler {
@@ -14,6 +16,12 @@ public class AssetHandler {
     public static void load() {
         assetManager.load("backGround/login_backGround.png", Texture.class);
         assetManager.load("badlogic.jpg", Texture.class);
+        assetManager.load("music/login.mp3", Music.class);
+        assetManager.load("sfx/click.mp3", Sound.class);
+        assetManager.load("button/login1.png", Texture.class);
+        assetManager.load("button/login2.png", Texture.class);
+        assetManager.load("button/signUp1.png", Texture.class);
+        assetManager.load("button/signUp2.png", Texture.class);
     }
 
 }
--- core/src/graphic/screen/LoginScreen.java
+++ core/src/graphic/screen/LoginScreen.java
@@ -2,6 +2,8 @@ package graphic.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +11,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import graphic.main.AssetHandler;
+import graphic.main.Button;
 import graphic.main.Main;
 
 import java.awt.*;
@@ -19,6 +22,10 @@ public class LoginScreen extends Screen {
     private String userName;
     private String password;
     private String confirmPassword;
+    private boolean isLogin;
+    private Button loginButton;
+    private Button signUpButton;
+    private Music music;
     private ShapeRenderer shapeRenderer;
     private Vector2 mousePos;
 
@@ -30,13 +37,32 @@ public class LoginScreen extends Screen {
         viewport.apply();
         camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
 
+        music = AssetHandler.getData().get("music/login.mp3");
+        music.setLooping(true);
+        music.play();
         backGround = AssetHandler.getData().get("backGround/login_backGround.png");
         shapeRenderer = new ShapeRenderer();
         mousePos = new Vector2();
+
+        userName = "";
+        password = "";
+        confirmPassword = "";
+
+
+        Texture temp1 = AssetHandler.getData().get("button/login1.png");
+        float x = (800 - (2 * temp1.getWidth())) / 2f;
+        float y = Main.HEIGHT - (100 + temp1.getHeight());
+        loginButton = new Button("button/login1.png", "button/login2.png", "sfx/click.mp3",x, y);
+
+        x = temp1.getWidth() + x;
+        signUpButton = new Button("button/signUp1.png", "button/signUp2.png", "sfx/click.mp3",x, y);
+        signUpButton.setActive(true);
+
     }
 
     @Override
     public void update() {
+
         camera.update();
         mousePos.set(Gdx.input.getX(), Gdx.input.getY());
         mousePos = viewport.unproject(mousePos);
@@ -60,6 +86,14 @@ public class LoginScreen extends Screen {
 
             @Override
             public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                if (loginButton.contains(mousePos)) {
+                    loginButton.setActive(true);
+                    signUpButton.setActive(false);
+                }
+                else if (signUpButton.contains(mousePos)) {
+                    loginButton.setActive(false);
+                    signUpButton.setActive(true);
+                }
                 return false;
             }
 
@@ -95,11 +129,20 @@ public class LoginScreen extends Screen {
         batch.draw(backGround, 0, 0);
         batch.end();
 
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Main.toColor(new Color(0x24F3F6F1, true)));
+        shapeRenderer.rect(0, 0, 800, 900);
+        shapeRenderer.end();
+
+        loginButton.draw(batch);
+        signUpButton.draw(batch);
+
 
     }
 
     @Override
     public void dispose() {
 
+        music.dispose();
     }
 }
