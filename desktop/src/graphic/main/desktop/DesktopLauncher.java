a48c86be09fb451fe70820decb6f44a47653dcca
erfanmotnt <moeini.erfan@gmail.com>
2019-06-15 01:12:08 +0430

    comit

--- desktop/src/graphic/main/desktop/DesktopLauncher.java
+++ desktop/src/graphic/main/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import graphic.main.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1600;
-		config.height = 900;
+		config.width = 1;
+		//config.height = 900;
 		config.fullscreen = true;
 		config.resizable = false;
 		config.forceExit = true;
