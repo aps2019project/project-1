ca2d6af7366453eb86165b062ec910403b2ca080
sadraheydari <msadrah521@gmail.com>
2019-06-15 01:06:16 +0430

    debug size

--- desktop/src/graphic/main/desktop/DesktopLauncher.java
+++ desktop/src/graphic/main/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import graphic.main.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1600;
-		config.height = 900;
+		config.width = 450;
+		config.height = 450;
 		config.fullscreen = true;
 		config.resizable = false;
 		config.forceExit = true;
