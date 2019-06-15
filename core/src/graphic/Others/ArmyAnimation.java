22c66370b22c723831026b564cb17379994b0c7f
sadraheydari <msadrah521@gmail.com>
2019-06-15 23:32:21 +0430

    create ArmyAnimation class

---
+++ core/src/graphic/Others/ArmyAnimation.java
@@ -0,0 +1,25 @@
+package graphic.Others;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import graphic.main.Gif;
+
+import javax.xml.soap.Text;
+
+public class ArmyAnimation {
+
+    private Gif normalGif;
+    private Gif attackGif;
+    private Gif deathGif;
+    private Gif runGif;
+    private Sound attackSound;
+    private Sound deathSound;
+    private Sound runSound;
+    private Texture infoPic;
+    private BitmapFont font;
+    private MoveAnimation runAnimation;
+
+
+
+}
