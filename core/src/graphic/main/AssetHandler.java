abd87aaca204fc6c2a26fb1a9b7603f88625ae08
sadraheydari <msadrah521@gmail.com>
2019-06-19 20:30:27 +0430

    add shop button

--- core/src/graphic/main/AssetHandler.java
+++ core/src/graphic/main/AssetHandler.java
@@ -68,6 +68,12 @@ public class AssetHandler {
         assetManager.load("battle/Tile.png", Texture.class);
         assetManager.load("button/yellow.png", Texture.class);
         assetManager.load("button/yellow glow.png", Texture.class);
+        assetManager.load("button/shop left.png", Texture.class);
+        assetManager.load("button/shop left active.png", Texture.class);
+        assetManager.load("button/shop middle.png", Texture.class);
+        assetManager.load("button/shop middle active.png", Texture.class);
+        assetManager.load("button/shop right.png", Texture.class);
+        assetManager.load("button/shop right active.png", Texture.class);
 
         /*for (int i = 1; i < 10; ++i) {
             assetManager.load("fonts/" + i + ".ttf", TrueTypeFont.class);
--- core/src/graphic/screen/TestScreen.java
+++ core/src/graphic/screen/TestScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -11,18 +12,26 @@ import graphic.Others.CardTexture;
 import graphic.Others.MoveAnimation;
 import graphic.Others.MoveType;
 import graphic.main.AssetHandler;
+import graphic.main.Button;
+import graphic.main.Main;
 import model.cards.Card;
 import model.cards.Minion;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class TestScreen extends Screen {
 
-    private ShapeRenderer shapeRenderer;
     private Vector2 mousePos;
     private Texture backGround;
     private Texture middleGround;
     private Texture forGround;
+    private Button heroButton;
+    private Button minionButton;
+    private Button spellButton;
+    private Button itemButton;
+    private Button sellButton;
+    private Button buyButton;
     private ArrayList<Minion> allCards;
     private ArrayList<CardTexture> cardTextures;
     private int startListFrom = 0;
@@ -39,20 +48,11 @@ public class TestScreen extends Screen {
         allCards = Card.getCards().getAllMinions();
         playBackGroundMusic("music/shop.mp3");
 
-        snowAnimation = new ArrayList<MoveAnimation>();
-        for (int i = 0; i < 100; ++i) {
-            int xStart = (int) (400 + 1200 * Math.random());
-            int xEnd = (int) (xStart - 150 + 300 * Math.random());
-            int random = (int) (Math.random() * 5);
-            if (random > 3)
-                snowAnimation.add(new MoveAnimation("animation/snow2.png", xStart, 900, xEnd, 0, MoveType.SIMPLE, true));
-            else
-                snowAnimation.add(new MoveAnimation("animation/snow.png", xStart, 900, xEnd, 0, MoveType.SIMPLE, true));
-
-            snowAnimation.get(i).setSpeed((float) (0.2f + Math.random()));
-        }
+        createSnowAnimation();
+        createButtons();
 
         cardTextures = new ArrayList<CardTexture>();
+
 
 
     }
@@ -96,6 +96,13 @@ public class TestScreen extends Screen {
 
             @Override
             public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                if (heroButton.contains(mousePos) || minionButton.contains(mousePos) || spellButton.contains(mousePos) || itemButton.contains(mousePos)) {
+                    heroButton.setActive(heroButton.contains(mousePos));
+                    minionButton.setActive(minionButton.contains(mousePos));
+                    spellButton.setActive(spellButton.contains(mousePos));
+                    itemButton.setActive(itemButton.contains(mousePos));
+                }
+
                 return false;
             }
 
@@ -124,11 +131,46 @@ public class TestScreen extends Screen {
 
     @Override
     public void render(SpriteBatch batch) {
-
         batch.setProjectionMatrix(camera.combined);
+
         drawBackGround(batch);
 
+        heroButton.draw(batch);
+        minionButton.draw(batch);
+        spellButton.draw(batch);
+        itemButton.draw(batch);
 
+
+    }
+
+    @Override
+    public void dispose() {
+        music.stop();
+        music.dispose();
+
+    }
+
+    private void createSnowAnimation() {
+        snowAnimation = new ArrayList<MoveAnimation>();
+        for (int i = 0; i < 100; ++i) {
+            int xStart = (int) (400 + 1200 * Math.random());
+            int xEnd = (int) (xStart - 150 + 300 * Math.random());
+            int random = (int) (Math.random() * 5);
+            if (random > 3)
+                snowAnimation.add(new MoveAnimation("animation/snow2.png", xStart, 900, xEnd, 0, MoveType.SIMPLE, true));
+            else
+                snowAnimation.add(new MoveAnimation("animation/snow.png", xStart, 900, xEnd, 0, MoveType.SIMPLE, true));
+            snowAnimation.get(i).setSpeed((float) (0.2f + Math.random()));
+        }
+    }
+
+    private void createButtons() {
+        BitmapFont font = AssetHandler.getData().get("fonts/Arial 24.fnt");
+        font.setColor(Main.toColor(new Color(0xFF7E05)));
+        heroButton = new Button("button/shop left.png", "button/shop left active.png", 300, 700, "Hero", font);
+        minionButton = new Button("button/shop middle.png", "button/shop middle active.png", 500, 700, "Minion", font);
+        spellButton = new Button("button/shop middle.png", "button/shop middle active.png", 700, 700, "Spell", font);
+        itemButton = new Button("button/shop right.png", "button/shop right active.png", 900, 700, "Item", font);
     }
 
     private void drawBackGround(SpriteBatch batch) {
@@ -141,12 +183,5 @@ public class TestScreen extends Screen {
         batch.begin();
         batch.draw(forGround, 0, 0);
         batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        music.stop();
-        music.dispose();
-
     }
 }
