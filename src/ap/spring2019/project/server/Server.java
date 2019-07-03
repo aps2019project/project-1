5c2bc0e69663692fde3361d3b950eab884303bd9
sadraheydari <msadrah521@gmail.com>
2019-07-04 01:19:42 +0430

    add Server main code

---
+++ src/ap/spring2019/project/server/Server.java
@@ -0,0 +1,70 @@
+package ap.spring2019.project.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private static final int PORT = 8000;
+    private static ServerSocket server;
+    private static HashMap<String, Socket> onlineUsers;
+    private static ArrayList<ap.spring2019.project.logic.model.game.Game> games;
+
+    static {
+        try {
+            server = new ServerSocket(PORT);
+            onlineUsers = new HashMap<>();
+            games = new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService serverSocketAdder = Executors.newSingleThreadExecutor();
+        serverSocketAdder.submit(new SocketAdder());
+    }
+
+    static synchronized ServerSocket getServer() {
+        return server;
+    }
+
+    static synchronized HashMap<String, Socket> getOnlineUsers() {
+        return onlineUsers;
+    }
+
+    static synchronized ArrayList<ap.spring2019.project.logic.model.game.Game> getGames() {
+        return games;
+    }
+
+    static synchronized void addUser(String userName, Socket socket) {
+        onlineUsers.put(userName, socket);
+    }
+
+    static synchronized Socket deleteUser(String userName) {
+        try {
+            return onlineUsers.remove(userName);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    static synchronized void addGame(ap.spring2019.project.logic.model.game.Game game) {
+        games.add(game);
+    }
+
+    static synchronized void deleteGame(ap.spring2019.project.logic.model.game.Game game) {
+        try {
+            games.remove(game);
+        }
+        catch (Exception ignored){}
+    }
+
+}
