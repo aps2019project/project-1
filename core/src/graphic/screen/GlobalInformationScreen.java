ef32a7f918d91fe5bb18b4652e3c722daff2d7ec
sadraheydari <msadrah521@gmail.com>
2019-07-07 22:37:00 +0430

    create Global Information Screen

---
+++ core/src/graphic/screen/GlobalInformationScreen.java
@@ -0,0 +1,25 @@
+package graphic.screen;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GlobalInformationScreen extends Screen {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
