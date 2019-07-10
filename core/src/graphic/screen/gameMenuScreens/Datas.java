b0fcb83624618ef73251fd5de8c3dec8cd115249
sadraheydari <msadrah521@gmail.com>
2019-07-10 22:05:21 +0430

    add matchResult

--- core/src/graphic/screen/gameMenuScreens/Datas.java
+++ core/src/graphic/screen/gameMenuScreens/Datas.java
@@ -133,6 +133,7 @@ public class Datas {
 //            ScreenManager.setScreen(new MenuScreen());
         } else {
             MatchResult result = game.getResults();
+            Account.getCurrentAccount().addMatchResult(result);
 //            System.out.println(account.getDaric());
 //            System.out.println(secondAccount.getDaric());
             game.getWinner().increaseDaric(1000);
